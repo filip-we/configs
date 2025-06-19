9f7e0a18f0506d9c147a2758b1a17655129a7de0
filip-wettlen-inkga <filip.wettlen1@ingka.ikea.com>
2025-06-19 17:10:38 +0200

    Fixed vim-airline not displayed correctly

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -24,7 +24,7 @@ set ruler
 set t_Co=256
 syntax on
 set hlsearch
-colorscheme filipwe
+"colorscheme filipwe
 
 " Syntax for asm6502
 augroup asm6502
@@ -37,9 +37,6 @@ set wildignore=*/.git/*,*/*venv*/*,*/__pycache__/*,*/temp_test/*
 
 " Set netwr to be sticky to the left.
 " let g:netrw_altv=1
-
-" Allow vim-airline to find correct fonts
-let g:airline_powerline_fonts = 1
 
 " Allow setting dark/light theme from environment variable
 if empty($BACKGROUND)
@@ -55,11 +52,38 @@ else
     let g:airline_theme='base16_gruvbox_dark_soft'
 endif
 
+" Allow vim-airline to find correct fonts
+let g:airline_powerline_fonts = 1
+
+" Allow vim-airline to be displayed correctly
+let g:airline_left_sep = '»'
+let g:airline_left_sep = '▶'
+let g:airline_right_sep = '«'
+let g:airline_right_sep = '◀'
+let g:airline_symbols.linenr = '␊'
+let g:airline_symbols.linenr = '␤'
+let g:airline_symbols.linenr = '¶'
+let g:airline_symbols.branch = '⎇'
+let g:airline_symbols.paste = 'ρ'
+let g:airline_symbols.paste = 'Þ'
+let g:airline_symbols.paste = '∥'
+let g:airline_symbols.whitespace = 'Ξ'
+
+let g:airline_left_sep = ''
+let g:airline_left_alt_sep = ''
+let g:airline_right_sep = ''
+let g:airline_right_alt_sep = ''
+let g:airline_symbols.branch = ''
+let g:airline_symbols.readonly = ''
+let g:airline_symbols.linenr = ''
+
 " Allow swtiching pane (to Tmux) from Netrw with <C-l>
 nmap <leader><leader><leader><leader><leader><leader>l <Plug>NetrwRefresh
 
 " Testing search dirs
 set path+=**
+
+colorscheme filipwe
 
 ]])
 
