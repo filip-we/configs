0165eee1bb0c6e243f4e9c8eb54f1c1e49535105
filip-we <filip.wettlen@gmail.com>
2025-11-08 22:17:48 +0100

    feat: nvim remap for colemak

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -63,8 +63,19 @@ else
     let g:airline_theme='base16_gruvbox_dark_soft'
 endif
 
+" Check keyboard layout
+if empty($KEYBOARD_LAYOUT)
+    let keyboard_layout = "qwerty"
+else
+    let keyboard_layout = $KEYBOARD_LAYOUT
+endif
+
 " Remapped keys
+" Escape built-in termial easier
 tnoremap <Esc> <C-\><C-n>
+
+" Disable spawning empty buffer
+nnoremap <C-w><C-n> <nop>|xnoremap <C-w><C-n> <nop>
 
 " Allow vim-airline to find correct fonts
 let g:airline_powerline_fonts = 1
@@ -96,6 +107,44 @@ nmap <leader><leader><leader><leader><leader><leader>l <Plug>NetrwRefresh
 
 " Testing search dirs
 set path+=**
+
+" Remap if we use Colemak
+if keyboard_layout=='colemak'
+    " Cursor movement
+    noremap n h|xnoremap n h|onoremap n h
+    noremap e j|xnoremap e j|onoremap e j
+    noremap i k|xnoremap i k|onoremap i k
+    noremap o l|xnoremap o l|onoremap o l
+
+    " Move other stuff
+    noremap h e
+    noremap j o
+    noremap k n
+    noremap l i
+
+    " Text objects
+    onoremap l i
+    vnoremap l i
+
+    " Insert
+    nnoremap l i
+    nnoremap L I
+
+    " Search
+    nnoremap k n|xnoremap k n|onoremap k n
+    nnoremap K N|xnoremap K N|onoremap K N
+
+    " Window navigation
+    nnoremap <C-w>n <C-w>h|xnoremap <C-w>n <C-w>h
+    nnoremap <C-w>e <C-w>j|xnoremap <C-w>e <C-w>j
+    nnoremap <C-w>i <C-w>k|xnoremap <C-w>i <C-w>k
+    nnoremap <C-w>o <C-w>l|xnoremap <C-w>o <C-w>l
+    nnoremap <C-w>N <C-w>H|xnoremap <C-w>n <C-w>h
+    nnoremap <C-w>E <C-w>J|xnoremap <C-w>e <C-w>j
+    nnoremap <C-w>I <C-w>K|xnoremap <C-w>i <C-w>k
+    nnoremap <C-w>O <C-w>L|xnoremap <C-w>o <C-w>l
+
+endif
 
 " Set this last so that vim-airline colors work correctly
 colorscheme filipwe
--- home/.zshenv
+++ home/.zshenv
@@ -12,6 +12,9 @@ export DEFAULT_USER="filip"
 # My custom method to tell configs wether to use dark or light themes
 export BACKGROUND="dark"
 
+# Keyremapping
+export KEYBOARD_LAYOUT="colemak"
+
 # ZSH
 export ZSH_THEME="filipwe"
 export ZSH="$HOME/.config/zsh/ohmyzsh"
